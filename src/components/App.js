4b40c1e0af3fc160048fc2622aa52395c9957751
RahulReady <52625075+RahulReady@users.noreply.github.com>
2021-11-06 19:25:25 -0500

    wise indeed 1

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Oeing Click Click.</h1>
+      <h3>- Two Wise Men</h3>
+    </div>
+  );
+};
+
+export default App;
